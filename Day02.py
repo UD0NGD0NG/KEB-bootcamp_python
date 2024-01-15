6f0bf00f88e027fa3b70ef7b6f9cf579f0db14a1
udongdong <kimdongkyun0411@gmail.com>
2024-01-15 11:38:35 +0900

    Ver0.3 Day02_Type

---
+++ Day02.py
@@ -0,0 +1,11 @@
+a = "univ"
+print(a)
+print(a[0])
+#a[0] = 'U' # immutable
+print(a)
+
+b = ['Python', 'C++', 'Linux', 'Datastructure', 'Database']
+print(b)
+print(b[3])
+b[3] = 'Datastructure && Algorithms' # mutable
+print(b)
