b9135874500a87847852d9d5923ba380c8687853
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 14:43:24 +0900

    Ver2.1 Prime_Basic

--- Day03.py
+++ Day03.py
@@ -1,2 +1,12 @@
-subjects = { 'Python': "Kim", 'C++': "Sung", 'Datastructure': "Kim", 'Database': "Kang" }
-print("{0[Python]} {0[C++]}".format(subjects))
+number = int(input("Input number : "))
+cnt = 0
+i = 1
+while  i <= number:
+    if number % i == 0:
+        cnt = cnt + 1
+    i = i + 1
+
+if cnt == 2:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is NOT prime number')
