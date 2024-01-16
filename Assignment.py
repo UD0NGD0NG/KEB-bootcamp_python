e7424058c1489845caf584e9adb23993ad1f3c3e
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 19:26:06 +0900

    Ver3.2 Day03_Assignment 2-2

--- Assignment.py
+++ Assignment.py
@@ -1,3 +1,12 @@
-l = [3, 2, 1, 0]
-for val in l:
-    print(val)
+guess_me = 7
+number = 1
+while True:
+    if guess_me > number:
+        print('too low')
+    elif guess_me == number:
+        print('found it!')
+        break
+    else:
+        print('oops')
+        break
+    number += 1
