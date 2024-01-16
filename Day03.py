5398d095de3209036c9f3d89f0a038bce8cac137
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 14:44:35 +0900

    Ver2.2 Prime_Except 1 && number

--- Day03.py
+++ Day03.py
@@ -1,12 +1,12 @@
 number = int(input("Input number : "))
 cnt = 0
-i = 1
-while  i <= number:
+i = 2
+while  i < number:
     if number % i == 0:
         cnt = cnt + 1
     i = i + 1
 
-if cnt == 2:
+if cnt == 0:
     print(f'{number} is prime number')
 else:
     print(f'{number} is NOT prime number')
