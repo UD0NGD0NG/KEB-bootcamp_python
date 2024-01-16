5b08a666acc5302fb4d457d7f7b82eff6b07930a
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 10:59:30 +0900

    Ver1.7 Day03_split

--- Day03.py
+++ Day03.py
@@ -1,25 +1,14 @@
-university = "Inha\nUniversity"
-runiversity = r"Inha\nUniversity" # raw string
-print(university)
-print(runiversity)
+course = "2024 KEB Bootcamp"
+print(course)
+list_course = course.split() # default delimeter is ' '(white space)
+list_courseB = course.split('B') # set delimeter
+print(list_course)
+print(list_courseB)
 
-# slicing [start : end : step]
-print(university[:4]) # slicing
-print(university[:-11]) # reverse slicing
-print(university[:len(university)]) # len : return number of letters
+numbers = input("FirstNumber SecondNumber : ").split()
+print(numbers[0] + numbers[1]) # concatenation
+print(int(numbers[0]) + int(numbers[1])) # arithmetic operation
 
-number1 = input("First number : ")
-number2 = input("Second number : ")
-print(number1 + number2) # concatenation
-print(number1 * 3) # loop
-#print(number1 + 3)  # Error!
-
-String = "abcdefghijklmnopqrstuvwxyz"
-print(String[0]) # 0-based
-print(String[1])
-print(String[25])
-print(String[-1]) # reverse(1-based)
-print(String[-26])
-
-print(String.replace('a','A')) # String is immutable, but Function can change
-print(String) # Change by Function is temporary
+subjects = ["Python", "C++", "Database"]
+subjects_string = '/'.join(subjects)
+print(subjects_string)
