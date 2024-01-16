f3f72c64e3f8e4c6e3492c3fffa68d99f67c5bdf
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 15:31:34 +0900

    Ver2.9 Prime_Size Comparison

--- Day03.py
+++ Day03.py
@@ -2,6 +2,9 @@ numbers = input("Input start number finish number: ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
+if n1 > n2: # tuple packing
+    n1, n2 = n2, n1
+
 for number in range(n1, n2 + 1):
     isPrime = True
     if number < 2:
