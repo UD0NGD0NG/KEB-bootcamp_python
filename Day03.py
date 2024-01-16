528cd20f0bc9a72eba893c6c7a24e7649a0bf0e5
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 14:47:25 +0900

    Ver2.5 Prime_Exception

--- Day03.py
+++ Day03.py
@@ -1,13 +1,16 @@
 number = int(input("Input number : "))
 isPrime = True
-i = 2
-while  i < number:
-    if number % i == 0:
-        isPrime = False
-        break
-    i = i + 1
-
-if isPrime:
-    print(f'{number} is prime number')
-else:
+if number < 2:
     print(f'{number} is NOT prime number')
+else:
+    i = 2
+    while  i < number:
+        if number % i == 0:
+            isPrime = False
+            break
+        i = i + 1
+
+    if isPrime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is NOT prime number')
