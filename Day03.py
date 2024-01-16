ca0f43a4fc64548c85a771bc2387a94bfe6d9a7c
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 15:09:00 +0900

    Ver2.7 Day03_For vs While

--- Day03.py
+++ Day03.py
@@ -1,14 +1,16 @@
-number = int(input("Input number : "))
-isPrime = True
-if number < 2:
-    print(f'{number} is NOT prime number')
-else:
-    for i in range(2, number):
-        if number % i == 0:
-            isPrime = False
-            break
+univ = "inha"
+i = 0
+while i < len(univ):
+    print(univ[i], end=' ')
+    i = i + 1
 
-    if isPrime:
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is NOT prime number')
+print()
+
+for letter in univ:
+    print(letter, end=' ')
+
+print()
+
+# default range == (0, essential, 1)
+for k in range(0, len(univ), 1): # == for k in range(len(univ)):
+    print(univ[k], end=' ')
