d2b6e48158c656bbaee8055d1bce8656eba02be9
udongdong <kimdongkyun0411@gmail.com>
2024-01-16 13:17:46 +0900

    Ver1.9 find vs index

--- Day03.py
+++ Day03.py
@@ -1,20 +1,14 @@
-course = "2024 KEB Bootcamp"
-print(course.replace('KEB', 'Inha'))
-print(course)
-course = course.replace('KEB', 'Inha')
-print(course)
-
-str = "a djs aa sf a fad"
-print(str.replace('a', 'A')) # All
-str = str.replace('a', 'A', 4) # limit
-print(str)
-
-world = "!!!!!earth!!!!!earth!!!!!"
-print(world)
-print(world.strip('!')) # default delimeter is ' '(ws)
-print(world.rstrip('!'))
-print(world.lstrip('!'))
-print(world.find('earth')) # if value is exist find == index
-print(world.rindex('earth'))
-print(world.find('?')) # if value is not exist return -1
-#print(world.index('?'))  # Error!
+subjects = "Python C++ Database Linux"
+subject = input("수강신청과목 입력 : ")
+if (subjects.find(subject) != -1):
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.")
+else:
+    print("해당 과목이 존재하지 않습니다")
+    
+#index로 구현을 위해서는 try except구문 사용
+# preview
+subject = input("수강신청과목 입력 : ")
+try:
+    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
+except ValueError:
+    print('해당과목이 존재하지 않습니다')
