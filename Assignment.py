f8fb15496f82b27010ca205876d5c21655f85896
udongdong <kimdongkyun0411@gmail.com>
2024-01-17 16:55:18 +0900

    Ver4.1 Day04_Assignment

--- Assignment.py
+++ Assignment.py
@@ -1,10 +1,37 @@
-guess_me = 5
-for number in range(10):
-    if guess_me > number:
-        print('too low')
-    elif guess_me == number:
-        print('found it!')
-        break
-    else:
-        print('oops')
-        break
+#1
+e2f = dict(dog='chien', cat='chat', walrus='morse')
+
+#2
+print(e2f.get('walrus'))
+
+#3
+f2e = {}
+for value in e2f.items():
+    f2e[value[1]] = value[0]
+
+#4
+for value in e2f.items():
+    if value[1] == 'chien':
+        print(value[0])
+
+#5
+print(e2f.keys())
+
+#6
+life = {
+    'animals': {'cats':'Henri', 'octopi':'Grumpy', 'emus':'Lucy'},
+    'plants': {},
+    'other': {}
+}
+
+#7
+print(life.keys())
+
+#8
+print(life['animals'].keys())
+
+#9
+print(life['animals']['cats'])
+
+#10
+squares = {key: key * key for key in range(10)}
