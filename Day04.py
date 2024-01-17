9840ef3440c9381b613b64d1fd3c2af1f437d5e5
udongdong <kimdongkyun0411@gmail.com>
2024-01-17 13:37:26 +0900

    Ver3.7 Day04_List_Copy

--- Day04.py
+++ Day04.py
@@ -1,6 +1,17 @@
-l1 = [1, 3, 6, 12, 612, 2]
-l2 = sorted(l1) # origin list isn't change
-print(l1, l2)
-l1.sort() # Asc
-l2.sort(reverse=True) # Desc
-print(l1, l2)
+# shallow copy
+l1 = [1, 2, 3] # Values in list are immutable
+l2 = l1
+l3 = l1.copy()
+l4 = list(l1)
+l5 = l1[:]
+l1[1] = 20
+print(l1, l2, l3, l4, l5)
+
+# deep copy
+ll1 = [1, [2, 3], 4] # ll1[1] is mutable
+ll2 = ll1
+ll3 = ll1.copy()
+ll4 = list(ll1)
+ll5 = ll1[:]
+ll1[1][0] = 20
+print(ll1, ll2, ll3, ll4, ll5)
