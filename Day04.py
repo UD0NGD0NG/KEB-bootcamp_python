b560656f012d1a00559d275674365089cb02c350
udongdong <kimdongkyun0411@gmail.com>
2024-01-17 13:52:42 +0900

    Ver3.8 Day04_List_Comprehension

--- Day04.py
+++ Day04.py
@@ -1,17 +1,5 @@
-# shallow copy
-l1 = [1, 2, 3] # Values in list are immutable
-l2 = l1
-l3 = l1.copy()
-l4 = list(l1)
-l5 = l1[:]
-l1[1] = 20
-print(l1, l2, l3, l4, l5)
+squares = [i * i for i in range(1, 6, 1)]
+print(squares)
 
-# deep copy
-ll1 = [1, [2, 3], 4] # ll1[1] is mutable
-ll2 = ll1
-ll3 = ll1.copy()
-ll4 = list(ll1)
-ll5 = ll1[:]
-ll1[1][0] = 20
-print(ll1, ll2, ll3, ll4, ll5)
+even_squares = [i * i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)
