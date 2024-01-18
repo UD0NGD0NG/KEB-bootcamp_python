cd8e4372fd4cfe9724a1a5595608aeae6477fb3b
udongdong <kimdongkyun0411@gmail.com>
2024-01-18 15:34:06 +0900

    Ver4.9 Day05_generator

--- Day05.py
+++ Day05.py
@@ -1,6 +1,21 @@
-#lambda value: set value code)
-z = lambda x: x ** 2  # assign address
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(list(map(z, l))) # using lambda's address
+def my_range(first=0, last=10, step=1):
+    number = first
+    while number < last:
+        yield number # remember just before
+        number += step
 
-print(list(map(lambda y: y ** 3, l))) # using lambda
+ranger = my_range(1, 5)
+
+for x in ranger:
+    print(x)
+
+for x in ranger: # print nothing
+    print(x)
+
+# comprehension
+gen_obj = (num for num in range(1, 5))
+for x in gen_obj:
+    print(x)
+
+for x in gen_obj: # print nothing
+    print(x)
