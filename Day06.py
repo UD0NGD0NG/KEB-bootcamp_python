9c6d75064b2eac0b9a15bcf19fcad009d0c795a3
udongdong <kimdongkyun0411@gmail.com>
2024-01-19 13:45:34 +0900

    Ver5.6 Day06_class

--- Day06.py
+++ Day06.py
@@ -1,9 +1,23 @@
-class Oops(Exception):
-    pass
-try:
-    print ("Good")
-    raise Oops("oops")
-except Oops as err:
-    print(f"{err}")
-except Exception: # catch all error (must be last)
-    print("Error occurs")
+class Pokemon:
+    # constructor
+    def __init__(self, name): # self-default, other-parameter
+        self.name = name
+        print(f"Create Pokemon {name}")
+
+    def attack(self, target):
+        print(f"{self.name} attacks {target.name}!")
+
+pikachu = Pokemon("Pikachu")
+squirtle = Pokemon("Squirtle")
+charizard = Pokemon("Charizard")
+
+pikachu.Lv = 10
+pikachu.nemesis = squirtle
+print(pikachu.Lv)
+#print(pikachu.nemesis.Lv)  # Error
+pikachu.nemesis.Lv = 9
+print(squirtle.Lv)
+
+print(charizard.name)
+
+charizard.attack(pikachu)
