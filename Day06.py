f42df989dd7fded242dcd0c467f8524b36d18c23
udongdong <kimdongkyun0411@gmail.com>
2024-01-19 14:43:16 +0900

    Ver5.7 Day06_inheritance

--- Day06.py
+++ Day06.py
@@ -1,23 +1,31 @@
 class Pokemon:
-    # constructor
-    def __init__(self, name): # self-default, other-parameter
+    def __init__(self, name):
         self.name = name
-        print(f"Create Pokemon {name}")
 
     def attack(self, target):
-        print(f"{self.name} attacks {target.name}!")
+        print(f"{self.name}이(가) {target.name}을(를) 공격!")
 
-pikachu = Pokemon("Pikachu")
-squirtle = Pokemon("Squirtle")
-charizard = Pokemon("Charizard")
+class Pikachu(Pokemon): # inherit(is-a)
+    def __init__(self, name, type):
+        super().__init__(name) # Delegation
+        self.type = type
 
-pikachu.Lv = 10
-pikachu.nemesis = squirtle
-print(pikachu.Lv)
-#print(pikachu.nemesis.Lv)  # Error
-pikachu.nemesis.Lv = 9
-print(squirtle.Lv)
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) {self.type} 공격!")
 
-print(charizard.name)
+    def attack_info(self):
+        print(f"{self.type} 계열의 공격을 합니다.")
 
-charizard.attack(pikachu)
+class Squirtle(Pokemon):
+    pass
+
+p1 = Pikachu("피카츄", "전기")
+p2 = Squirtle("꼬부기")
+p3 = Pokemon("미싱노")
+
+print(p1.name)
+p1.attack(p2)
+p2.attack(p1)
+
+p1.attack_info()
+#p3.attack_info()  # Error, attack_info is personality
