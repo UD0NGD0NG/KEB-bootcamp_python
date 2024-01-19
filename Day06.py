bcea0e58edd42f7b866864bcda13aa6f25d7d330
udongdong <kimdongkyun0411@gmail.com>
2024-01-19 11:19:30 +0900

    Ver5.5 Day06_raise

--- Day06.py
+++ Day06.py
@@ -1,15 +1,9 @@
-import random
-numbers = [random.randint(1, 100) for i in range(10)]
-
+class Oops(Exception):
+    pass
 try:
-    pick = int(input(f"Enter index (0 ~ {len(numbers) - 1}) : "))
-    print(numbers[pick])
-    print(5 / 0)
-except IndexError as err:
-    print(f"{err} : Wrong index number")
-except ValueError:
-    print("Enter only number")
-except ZeroDivisionError as err:
+    print ("Good")
+    raise Oops("oops")
+except Oops as err:
     print(f"{err}")
 except Exception: # catch all error (must be last)
     print("Error occurs")
